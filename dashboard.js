1bcfd2de32ae169db529c34c96329f916884bd9e
Quinton Angus <quinton@quintondev.com>
2026-09-05 05:17:11 +0100

    commit

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,25 @@
-async
+function disableLoading() {
+    const loading = document.getElementById("dashboardSpinner")
+    const content = document.getElementById("dashboardContent")
+
+    loading.style.display = "none"
+    content.style.display = "flex"
+}
+
+async function verify() {
+    const verifyRequest = await fetch("https://api.danielle-and-callum.quintondev.com/v1/auth/verify", {
+        method: "POST",
+        credentials: "include",
+        headers: {"Content-Type":"application/json"}
+    })
+
+    if (verifyRequest.status === 500) {
+        window.location.href = "./login.html"
+    }  else if (verifyRequest.status === 401) {
+        window.location.href = "./login.html"
+    } else {
+        disableLoading()
+    }
+}
+
+verify()
